42e1191e9da22683cfe222006c12b6b0dbb4c500
Mahin556 <mahinraza556@gmail.com>
2026-02-12 23:46:46 +0530

    fix vagrant file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,19 +2,23 @@
 
 # Enable ssh password authentication
 echo "[TASK 1] Enable ssh password authentication"
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-systemctl reload sshd
+sudo systemctl reload sshd
 
 # Set Root password
 echo "[TASK 2] Set root password"
-echo -e "root\nroot" | passwd root >/dev/null 2>&1
+sudo echo -e "root\nroot" | passwd root >/dev/null 2>&1
 
 # Create a ansible user and configure it
-useradd -m -s /bin/bash ansible
-mkdir -p /home/ansible/.ssh
-cat /tmp/ansible.pub >> /home/ansible/.ssh/authorized_keys
-chown -R ansible:ansible /home/ansible/.ssh
-chmod 700 /home/ansible/.ssh
-chmod 600 /home/ansible/.ssh/authorized_keys
-echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+sudo useradd -m -s /bin/bash ansible
+sudo mkdir -p /home/ansible/.ssh
+sudo cat /tmp/ansible.pub >> /home/ansible/.ssh/authorized_keys
+sudo chown -R ansible:ansible /home/ansible/.ssh
+sudo chmod 700 /home/ansible/.ssh
+sudo chmod 600 /home/ansible/.ssh/authorized_keys
+sudo echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+sudo mv /tmp/hosts /etc/hosts
+sudo chown root:root /etc/hosts
+sudo chmod 644 /etc/hosts
