c8a5f8ff34f0db510dd1d89411d420aa6ce0bebe
Mahin556 <mahinraza556@gmail.comm>
2026-02-14 14:22:03 +0000

    update the bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
 echo "[TASK 1] Install required packages"
-apt update -y
-apt install -y rsync
+sudo apt update -y
+sudo apt install -y rsync
 
 # Enable ssh password authentication
 echo "[TASK 1] Enable ssh password authentication"
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-systemctl reload sshd
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sudo systemctl reload sshd
 
 echo "[TASK 2] Create ansible user"
 
 # Create user only if not exists
-id -u ansible &>/dev/null || useradd -m -s /bin/bash ansible
+sudo id -u ansible &>/dev/null || useradd -m -s /bin/bash ansible
 
 # Setup SSH directory
-mkdir -p /home/ansible/.ssh
-chmod 700 /home/ansible/.ssh
+sudo mkdir -p /home/ansible/.ssh
+sudo chmod 700 /home/ansible/.ssh
 
 echo "[TASK 3] Configure SSH key authentication"
 
 # Add public key
-cat /tmp/ansible.pub >> /home/ansible/.ssh/authorized_keys
-chmod 600 /home/ansible/.ssh/authorized_keys
+sudo cat /tmp/ansible.pub >> /home/ansible/.ssh/authorized_keys
+sudo chmod 600 /home/ansible/.ssh/authorized_keys
 
 # Copy private key
-cp /tmp/ansible /home/ansible/.ssh/id_rsa
+sudo cp /tmp/ansible /home/ansible/.ssh/id_rsa
 chmod 600 /home/ansible/.ssh/id_rsa
 
 # Fix ownership
-chown -R ansible:ansible /home/ansible/.ssh
+sudo chown -R ansible:ansible /home/ansible/.ssh
 
 echo "[TASK 4] Configure passwordless sudo"
 
-echo "ansible ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ansible
-chmod 440 /etc/sudoers.d/ansible
+sudo echo "ansible ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ansible
+sudo chmod 440 /etc/sudoers.d/ansible
 
 echo "[TASK 5] Configure host resolution"
 
-mv /tmp/hosts /etc/hosts
-chown root:root /etc/hosts
-chmod 644 /etc/hosts
+sudo mv /tmp/hosts /etc/hosts
+sudo chown root:root /etc/hosts
+sudo chmod 644 /etc/hosts
 
 echo "Bootstrap complete."
