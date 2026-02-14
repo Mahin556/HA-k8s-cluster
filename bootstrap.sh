233ee162c7b2dbd5c3b1bf39424dc0631c14011b
Mahin556 <mahinraza556@gmail.comm>
2026-02-14 15:40:25 +0000

    update the vagrantfile and bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 
-echo "[TASK 1] Install required packages"
-sudo apt update -y
-sudo apt install -y rsync sshpass
-
 # Enable ssh password authentication
-echo "[TASK 1] Enable ssh password authentication"
 sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sudo systemctl reload sshd
 
 # Set Root password
-echo "[TASK 2] Set root password"
-echo -e "root\nroot" | passwd root >/dev/null 2>&1
+sudo echo -e "root\nroot" | passwd root >/dev/null 2>&1
+sudo echo 'ansible:ansible' | sudo chpasswd
 
-# Set ansible user password
-echo "[TASK 2] Set ansible user password"
-echo -e "ansible\nansible" | passwd ansible >/dev/null 2>&1
-
-echo "[TASK 2] Create ansible user"
-
-# Create user only if not exists
-sudo id -u ansible &>/dev/null || useradd -m -s /bin/bash ansible
+# Create a ansible user and configure it
+sudo useradd -m -s /bin/bash ansible
 
 # Setup SSH directory
 sudo mkdir -p /home/ansible/.ssh
 sudo chmod 700 /home/ansible/.ssh
-
-echo "[TASK 3] Configure SSH key authentication"
 
 # Add public key
 sudo cat /tmp/ansible.pub >> /home/ansible/.ssh/authorized_keys
@@ -36,15 +23,9 @@ sudo chmod 600 /home/ansible/.ssh/authorized_keys
 # Fix ownership
 sudo chown -R ansible:ansible /home/ansible/.ssh
 
-echo "[TASK 4] Configure passwordless sudo"
-
-echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
+sudo echo "ansible ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ansible
 sudo chmod 440 /etc/sudoers.d/ansible
-
-echo "[TASK 5] Configure host resolution"
 
 sudo mv /tmp/hosts /etc/hosts
 sudo chown root:root /etc/hosts
 sudo chmod 644 /etc/hosts
-
-echo "Bootstrap complete."
